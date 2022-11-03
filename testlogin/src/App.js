732af24c3a43c8c6ebf05c78b51f87f0e4359011
Sineth <sinethshashintha2000@gmail.com>
2022-11-03 17:10:22 +0530

    gave css styles

--- testlogin/src/App.js
+++ testlogin/src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import profile from "./image/1.png";
+import email from "./image/2.png";
+import pass from "./image/3.png";
 
 function App() {
   return (
     <div className='main'>
       <div className='sub-main'>
+        <div>
+          <div className='imgs'>
+            <div className='container-image'>
+              <img src={profile} alt="profile" className='profile' />
+            </div>
+          </div>
+          <div>
+            <h1>Login Page</h1>
+            <div>
+              <img src={email} alt="email" className='email' />
+              <input type="text" placeholder="user name" className='name'/>
+            </div>
+
+            <div>
+              <img src={pass} alt="pass" className='pass' />
+              <input type="text" placeholder="password" className='pass'/>
+            </div>
+          </div>
+        </div>
+
 
       </div>
     </div>
